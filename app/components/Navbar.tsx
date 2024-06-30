250a034516d61216ffea6b01548053bfe338054b
Anirudh Pratap Singh <110687427+anirudhpratap345@users.noreply.github.com>
2024-06-30 17:02:32 +0530

    message

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export function Navbar() {
+    return  (
+        <nav className="border-b bg-background h-[10vh] flex items-center">
+            <div className="container flex items-center justify-between">
+                <Link></Link>
+            </div>
+
+        </nav>
+    )
+}
